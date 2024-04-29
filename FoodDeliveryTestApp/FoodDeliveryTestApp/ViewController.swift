7a85d1154d923c0c155768a04aa56a7c60016833
Roman <romaholovai@gmail.com>
2024-04-29 12:43:22 +0200

    Created base project, deleted main.storyboard, and project ready for code developing UI

---
+++ FoodDeliveryTestApp/FoodDeliveryTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodDeliveryTestApp
+//
+//  Created by Roman on 29/04/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
