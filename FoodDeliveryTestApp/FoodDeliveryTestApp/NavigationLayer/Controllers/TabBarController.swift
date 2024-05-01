ef29a3a19bb6b5adb2949f14f3cb5ed3fc4e4f3e
Roman <romaholovai@gmail.com>
2024-05-01 14:47:08 +0200

    Added other coordinators and tabbarcontroller

---
+++ FoodDeliveryTestApp/FoodDeliveryTestApp/NavigationLayer/Controllers/TabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  TabBarController.swift
+//  FoodDeliveryTestApp
+//
+//  Created by Roman on 01/05/2024.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+    init(tabBarControllers: [UIViewController]) {
+        super.init(nibName: nil, bundle: nil)
+        for tab in tabBarControllers {
+            self.addChild(tab)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tabBar.backgroundColor = AppColors.background
+        tabBar.tintColor = AppColors.accentOrange
+    }
+
+}
---
+++ FoodDeliveryTestApp/FoodDeliveryTestApp/NavigationLayer/Coordinator/HomeCoordinator.swift
@@ -0,0 +1,22 @@
+//
+//  HomeCoordinator.swift
+//  FoodDeliveryTestApp
+//
+//  Created by Roman on 01/05/2024.
+//
+
+import UIKit
+
+class HomeCoordinator: Coordinator {
+    
+    override func start() {
+        let vc = ViewController()
+        vc.view.backgroundColor = .red
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    override func finish() {
+        print("AppCoordinator finished")
+    }
+
+}
